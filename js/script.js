4111e5ea738bb1f012eda5d16065bb70d5d33da8
arnoldbgm <arnold.gallegosm@gmail.com>
2023-01-30 19:38:37 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+let rawtext = document.getElementById('input-text');
+let outputText = document.getElementById('output-text');
+let btnCopy = document.getElementById('btn-copy');
+let noteLeft = document.getElementById('text-moving');
+let noteRight = document.getElementById('text-right');
+let enInput = document.getElementById('btn-encode');
+let decoutput = document.getElementById('btn-decode');
+const code = ['enter', 'imes', 'ober', 'ai', 'ufat'];
+const uncode = ['e', 'i', 'o', 'a', 'u'];
+
+function encodeTex() {
+  let text = rawtext.value.trim().replaceAll(/\s+/g, ' ');
+  let decodeTex = outputText;
+
+  if (isChecked(text)) {
+    decodeTex.value = '';
+    noteLeft.className = '';
+    noteRight.style.display = 'none';
+    btnCopy.style.display = 'flex';
+    decodeTex.className = 'right-textarea';
+    for (let i = 0; i < uncode.length; i++) {
+      text = text.replaceAll(uncode[i], code[i]);
+    }
+    decodeTex.value += text;
+  } else {
+    text.value = '';
+    noteLeft.className = 'show';
+    alert('solo minusculas sin acentos o simbolos');
+  }
+}
+
+function decodeTxt() {
+  let encodeTex = rawtext.value.trim().replaceAll(/\s+/g, ' ');
+  let decodeTex = outputText;
+
+  if (isChecked(encodeTex)) {
+    decodeTex.value = '';
+    noteLeft.className = '';
+    noteRight.style.display = 'none';
+    btnCopy.style.display = 'flex';
+    decodeTex.className = 'right-textarea';
+    for (let i = 0; i < uncode.length; i++) {
+      encodeTex = encodeTex.replaceAll(code[i], uncode[i]);
+    }
+    decodeTex.value = encodeTex;
+  } else {
+    decodeTex.value = '';
+    noteLeft.className = 'show';
+
+    alert('solo minusculas sin acentos o simbolos');
+  }
+}
+
+function copy() {
+  let encodeTex = rawtext;
+  let decodeTex = outputText;
+
+  encodeTex.value = '';
+  decodeTex.select();
+  document.execCommand('copy');
+  encodeTex.value = decodeTex.value;
+  alert('Copiado!');
+}
+function isChecked(text) {
+  return !/[^a-z\sñ]/.test(text);
+}
+
+enInput.addEventListener('click', encodeTex);
+decoutput.addEventListener('click', decodeTxt);
+btnCopy.addEventListener('click', copy);
